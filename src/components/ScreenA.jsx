ed0c85d80831dbb8f46c34cf56981723944c6aba
ENDO TAKUMI <endo.takumi.info@gmail.com>
2024-01-24 15:56:21 +0900

    Add: Alert message

--- src/components/ScreenA.jsx
+++ src/components/ScreenA.jsx
@@ -5,6 +5,28 @@ const ScreenA = () => {
   return (
     <div className="flex items-stretch gap-4">
       <div className="flex-1">
+        <div
+          role="alert"
+          className="rounded border-s-4 border-red-500 bg-red-50 p-4"
+        >
+          <strong className="block font-medium text-red-800">
+            {' '}
+            サイトリニューアル中{' '}
+          </strong>
+
+          <p className="mt-2 text-sm text-red-700">
+            現在、サイトリニューアル中です。ご迷惑をおかけしますが、しばらくお待ちください。
+            旧ポートフォリオは
+            <a
+              href="https://endotakumi.github.io/portfolio-2023/"
+              target="__blank"
+            >
+              こちら
+            </a>
+            。
+          </p>
+        </div>
+
         {/* ---------- */}
         <SectionComponent
           title="タイトル1"
