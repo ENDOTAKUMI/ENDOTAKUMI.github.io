9171ecae0457f6dc8d2fd214d2be82ddab9f3228
ENDO TAKUMI <endo.takumi.info@gmail.com>
2024-01-24 15:06:46 +0900

    Add: Create section component

--- src/components/ScreenA.jsx
+++ src/components/ScreenA.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import SectionComponent from './SectionComponent';
 
 const ScreenA = () => {
-  return <div>ScreenA</div>;
+  return (
+    <div className="flex items-stretch gap-4">
+      <div className="flex-1">
+        {/* ---------- */}
+        <SectionComponent
+          title="タイトル1"
+          detail="詳細...詳細...詳細..."
+          image1="./image-example.png"
+          image2="./image-example.png"
+          link="https://example.com"
+        />
+        <SectionComponent
+          title="タイトル2"
+          detail="詳細...詳細...詳細..."
+          image1="./image-example.png"
+          image2="./image-example.png"
+          link="https://example.com"
+        />
+        {/* ---------- */}
+      </div>
+    </div>
+  );
 };
+
 export default ScreenA;
